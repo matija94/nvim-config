c49b4547cf1ac23f5a6e6b280de2347f3155356f
Matija Lukovic <matija@rho.co>
2024-05-03 18:41:15 +0200

    updates

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -131,6 +131,7 @@ require("lazy").setup({
 		{
 			"nvim-neotest/neotest",
 			dependencies = {
+        "nvim-neotest/nvim-nio",
 				"nvim-lua/plenary.nvim",
 				"antoinemadec/FixCursorHold.nvim",
 				"nvim-treesitter/nvim-treesitter",
